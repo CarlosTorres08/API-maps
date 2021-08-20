ece99a4b3880fd3c129296d9beda3f4da33d576d
Carlos Henrique <carlos_henriquetorres12@hotmail.com>
2021-08-19 23:48:24 -0300

    codigo finalizado

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("funcionando")
+function calcular () {
+  let number1 = document.getElementById("num1").value
+  let number2 = document.getElementById("num2").value
+  let n = Number(number1)
+  let s = Number(number2)
+  let calcular = n + s
+  alert(calcular)
+  
+}
+document.getElementById('somar').addEventListener('click', (evento) => {
+    // callback aqui
+    calcular()
+    })
